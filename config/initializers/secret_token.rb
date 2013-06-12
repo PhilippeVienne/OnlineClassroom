9e0b3d71115fb409e981d66702c1014f50fe2372
Rails Developper <rails@RailsOnVM.(none)>
2013-06-12 15:45:53 +0200

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OnlineClassroom::Application.config.secret_token = '8916e6148876a12b22044bf1c0d03e54b911475482da6b5d66281c48af2af763e185ddaf57be0bb4fb81fe5f66f587bc7b145087f3e4789ea6a76414a87ace1b'
