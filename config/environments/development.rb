606b982af362e4daceea54fe0567fca2e7270079
Rails Developper <rails@RailsOnVM.(none)>
2013-06-14 09:22:36 +0200

    Configure Devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,8 @@ OnlineClassroom::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # EMails Development
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
 end
