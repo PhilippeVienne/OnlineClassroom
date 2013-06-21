692a29bc4f1278f3c8091b50eef68e7cd9b9cde1
Philippe VIENNE <PhilippeGeek@gmail.com>
2013-06-22 00:58:30 +0200

    Improve specs for User Registration and login

--- spec/resuests/Users_logic_spec.rb
+++ spec/resuests/Users_logic_spec.rb
@@ -1,30 +1,46 @@
 require 'spec_helper'
 
 describe 'Users Application Logic' do
-  describe "User Websit Registration" do
-    it "allows new users to register with an email address and password" do
-      visit "/users/sign_up"
+  describe 'User Website Registration' do
 
-      fill_in "Email",                 :with => "alindeman@example.com"
-      fill_in "Password",              :with => "ilovegrapes"
-      fill_in "Password confirmation", :with => "ilovegrapes"
-
-      click_button "Sign up"
-
-      page.should have_content("Welcome! You have signed up successfully.")
+    before(:each) do
+      @user=FactoryGirl.create(:user)
     end
-    it "allows users to sign in after they have registered" do
-      user = User.create(:email    => "alindeman@example.com",
-                         :password => "ilovegrapes")
 
-      visit "/users/sign_in"
+    it 'allows new users to register with an email address and password' do
+      visit '/users/sign_up'
 
-      fill_in "Email",    :with => "alindeman@example.com"
-      fill_in "Password", :with => "ilovegrapes"
+      fill_in 'Email',                 :with => 'alindeman@example.com'
+      fill_in 'Password',              :with => 'ilovegrapes'
+      fill_in 'Password confirmation', :with => 'ilovegrapes'
 
-      click_button "Sign in"
+      click_button 'Sign up'
 
-      page.should have_content("Signed in successfully.")
+      page.should have_content(I18n.translate('devise.registrations.signed_up'))
+    end
+
+    it 'not allows new users to register with an existent email address' do
+      visit '/users/sign_up'
+
+      fill_in 'Email',                 :with => @user.email
+      fill_in 'Password',              :with => 'ilovegrapes'
+      fill_in 'Password confirmation', :with => 'ilovegrapes'
+
+      click_button 'Sign up'
+
+      page.should_not have_content(I18n.translate('devise.registrations.signed_up'))
+    end
+
+    it 'allows users to sign in after they have registered' do
+
+      visit '/users/sign_in'
+
+      fill_in 'Email',    :with => @user.email
+      fill_in 'Password', :with => @user.password
+
+      click_button 'Sign in'
+
+      page.should have_content(I18n.translate('devise.sessions.signed_in'))
     end
   end
-    end
+end
